7c4d9c0444e28dff603d6441896d8ed82f103697
Dawid Draguła <46498046+Vazonik@users.noreply.github.com>
2023-09-19 10:27:25 +0200

    Fixed indents.

--- highcharts-api/highcharts-maps/2-highcharts-maps-overview/main.js
+++ highcharts-api/highcharts-maps/2-highcharts-maps-overview/main.js
@@ -1,22 +1,21 @@
 Highcharts.getJSON(
-    'https://code.highcharts.com/mapdata/custom/world-continents.geo.json',
-    geojson => {
+  'https://code.highcharts.com/mapdata/custom/world-continents.geo.json',
+  geojson => {
 
     const data = [
-            ['af', 10],
-            ['eu', 10],
-            ['sa', 10]
+      ['af', 10],
+      ['eu', 10],
+      ['sa', 10]
     ];
 
-      Highcharts.mapChart('container', {
-        series: [{
-          name: 'World map',
-          data,
-          type: 'map',
-          mapData: geojson,
-          showInLegend: false
-        }]
-      });
-    }
-  );
-  
+    Highcharts.mapChart('container', {
+      series: [{
+        name: 'World map',
+        data,
+        type: 'map',
+        mapData: geojson,
+        showInLegend: false
+      }]
+    });
+  }
+);
