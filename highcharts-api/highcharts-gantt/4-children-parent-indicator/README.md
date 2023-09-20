3150410fbbc7f2e13cb17e8434b0db680da3d8fa
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-09-20 08:47:43 +0200

    Added new exercise in gantt.

---
+++ highcharts-api/highcharts-gantt/4-children-parent-indicator/README.md
@@ -0,0 +1,5 @@
+# Gantt exercise 4
+
+Create a dotted box around the group of points, which are in the relation parent-children. Take a look at the gif for a reference.
+The box should start in the middle of the parent, and should go around all of the children.
+![gantt-exercise.gif](gantt-exercise.gif)
---
+++ highcharts-api/highcharts-gantt/4-children-parent-indicator/main.js
@@ -0,0 +1,23 @@
+Highcharts.ganttChart('container', {
+
+    series: [{
+        name: 'Project 1',
+        data: [{
+            name: 'Main',
+            pointWidth: 0
+        }, {
+            name: 'First',
+            start: 1560902400000,
+            end: 1561075200000,
+        }, {
+            name: 'Second',
+            start: 1560902400000,
+            end: 1561075200000
+        }, {
+            name: 'Second',
+            start: 1561507200000,
+            end: 1561680000000
+        }]
+    }]
+
+});
