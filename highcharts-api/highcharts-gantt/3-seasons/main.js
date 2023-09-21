22c6a196c43e081751e81f2b4fa618bd9d54ad69
mfilipiec <michalfilipiec@gmail.com>
2023-09-21 10:31:31 +0200

    Added gantt/03-seasons task

---
+++ highcharts-api/highcharts-gantt/3-seasons/main.js
@@ -0,0 +1,12 @@
+Highcharts.ganttChart('container', {
+  series: [
+    {
+      data: [
+        {
+          start: Date.UTC(2022, 4, 3),
+          end: Date.UTC(2022, 5, 2)
+        }
+      ]
+    }
+  ]
+});
