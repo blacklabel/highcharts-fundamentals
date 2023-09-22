fab25a194cfe256a62e366556d42bb8683bc9a42
mfilipiec <michalfilipiec@gmail.com>
2023-09-22 17:53:52 +0200

    Added stock/01-stock-initial task

---
+++ highcharts-api/highcharts-stock/01-stock-initial/main.js
@@ -0,0 +1,21 @@
+(async () => {
+  const data = await fetch(
+    'https://demo-live-data.highcharts.com/aapl-ohlcv.json'
+  ).then((response) => response.json());
+  Highcharts.chart('container', {
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: {
+      title: {
+        text: 'Value'
+      }
+    },
+    series: [
+      {
+        type: 'line',
+        data: data
+      }
+    ]
+  });
+})();
